880f92d2bb2d63b465c8343b0cf8bce74b1312cb
Nicholas Rose <35816886+nicholasgrose@users.noreply.github.com>
2024-01-28 17:32:22 -0600

    Implemented the new layout for the UI

---
+++ src/lib/navigation/NavMenu.svelte
@@ -0,0 +1,17 @@
+<nav class="nav-menu">
+    <button class="nav-button">Encode</button>
+    <button class="nav-button">Decode</button>
+    <button class="nav-button">About</button>
+</nav>
+
+<style>
+    .nav-menu {
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+    }
+
+    .nav-button:hover {
+        background: mediumaquamarine;
+    }
+</style>
